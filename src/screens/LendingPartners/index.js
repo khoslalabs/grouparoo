6bfbd19403367d5c3e7d8f2dba6b3cfeab18391e
jsaurabh2794-novopay <saurabh.kumar@novopay.in>
2022-04-19 12:48:07 +0530

    Mobile No Fix

--- src/screens/LendingPartners/index.js
+++ src/screens/LendingPartners/index.js
@@ -27,7 +27,7 @@ const LendingPartners = ({ navigation, route }) => {
         </View>
         <View style={styles.row}>
           <Text category='c1' appearance='hint'>{translations['lending.partner.help.desk']}: </Text>
-          <Text category='c2' appearance='default'>+91 73021186677</Text>
+          <Text category='c2' appearance='default'>+91 7302118667</Text>
         </View>
       </Card>
     </>
