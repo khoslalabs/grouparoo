98ab960e3c2c167748d5c96a0cbf12e3e5c564fb
sahuarju <97150755+sahuarju@users.noreply.github.com>
2022-09-28 13:47:12 +0530

    Modified Vehicle details file

--- src/screens/components/React-json-schema-form/components/extLibraries/JsonSchemaForm/fields/VehicleDetailsField/index.js
+++ src/screens/components/React-json-schema-form/components/extLibraries/JsonSchemaForm/fields/VehicleDetailsField/index.js
@@ -5,7 +5,7 @@ import {
   useStyleSheet,
   StyleService,
 } from "@ui-kitten/components";
-import React, { useEffect, useState,useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import DocumentUploadComponent from "../../common/DocumentUploadComponent";
@@ -15,59 +15,31 @@ import isEmpty from "lodash.isempty";
 import { useRequest } from "ahooks";
 import MaskedInput from "../../../textMask/text-input-mask";
 import { useFormContext } from "../../FormContext";
-import { Spinner } from '@ui-kitten/components'
-import IconUtil from '../../../../common/IconUtil';
-import Toast from 'react-native-toast-message'
-import ResourceFactoryConstants from '../../../../../services/ResourceFactoryConstants'
-import DataService from '../../../../../services/DataService'
-import { LocalizationContext } from '../../../../../translation/Translation'
-
-
-
-const validateNo = async (dispatch, gstin) => {
-  const resourseFactoryConstants = new ResourceFactoryConstants()
-
-  const url = `${resourseFactoryConstants.constants.registrationNo.verifyNo}`
-  try {
-    const res = await DataService.getData(url)
-    debugger
-    const response = res?.data
-    if (response.status === 'SUCCESS') {
-      await Promise.all([
-        dispatch.formDetails.setGstnData(response.data),
-        dispatch.formDetails.setIsGSTVerified('Yes')
-      ])
-      return { success: true }
-    } else {
-      console.log(response.message)
-      throw new Error('INVALID_REGISTRATION_NO_ENTERED')
-    }
-  } catch (e) {
-    if (e.message === 'INVALID_REGISTRATION_NO_ENTERED') {
-      throw e
-    } else {
-      throw new Error('CANNOT_REACH_REGISTRATION_NO_VALIDATION_SERVER')
-    }
-  }
-}
-
+import { Spinner } from "@ui-kitten/components";
+import IconUtil from "../../../../common/IconUtil";
+import Toast from "react-native-toast-message";
+import ResourceFactoryConstants from "../../../../../services/ResourceFactoryConstants";
+import DataService from "../../../../../services/DataService";
+import { LocalizationContext } from "../../../../../translation/Translation";
+import axios from "axios";
 
 const VehicleDetailsField = (props) => {
   const { theme } = useFormContext();
   const styles = useStyleSheet(themedStyles);
-  const { translations } = useContext(LocalizationContext)
-
+  const { translations } = useContext(LocalizationContext);
 
   const dispatch = useDispatch();
   const filledData = props.formData;
-  console.log("filledData", filledData);
+  // console.log("filledData", filledData);
   const state = useSelector((state) => state);
   const [selectBrandIndex, setSelectBrandIndex] = useState();
   const [selectModelIndex, setSelectModelIndex] = useState();
   const [selectDelearIndex, setSelectDelearIndex] = useState();
-  const [invalid, setInvalid] = useState(false)
+  const [invalid, setInvalid] = useState(false);
+  const [amount, setAmount] = useState()
 
-  const [no, setNo] = useState('')
+
+  const [no, setNo] = useState("");
 
   const selectedBrand = brands[selectBrandIndex?.row] || filledData?.brand;
   const selectedDealr = delears[selectDelearIndex?.row] || filledData?.dealer;
@@ -76,65 +48,95 @@ const VehicleDetailsField = (props) => {
     models = getModelsArray(selectedBrand, selectedDealr);
   }
   const selectedModel =
-    models[selectModelIndex?.row] || filledData.vehicleDetails;
+    models[selectModelIndex?.row] || filledData?.vehicleDetails;
   const vehicleType = state?.formDetails?.formData?.vehicleType
     ? state?.formDetails?.formData?.vehicleType
     : "New";
+  const validateNo = async (dispatch, no) => {
+    console.log("inside validateNo1");
+    const resourseFactoryConstants = new ResourceFactoryConstants()
 
-  console.log(
-    "vehicleType in vehicle details",
-    vehicleType,
-    state?.formDetails?.formData
-  );
+    const url = resourseFactoryConstants.constants.registrationNo.verifyNo
+    console.log("inside validateNo2", no);
+    console.log("no", no,url);
+    try {
+      console.log("inside try");
+      const res = await DataService.postData(url, {"rcRegnNo":no })
+
+      // const res = await axios.post(
+      //   "https://novo-adapter-uat.novopay.in/vahan-service/vahan/validate",
+      //   { rcRegnNo: no }
+      // );
+      console.log("response=====>", res.data);
+      const response = res?.data;
+      if (response.status === "SUCCESS") {
+        console.log("inside success");
+       
+        return { success: true };
+      } else {
+        console.log(response.message);
+        throw new Error("INVALID_REGISTRATION_NO_ENTERED");
+      }
+    } catch (e) {
+      console.log("inside catch", e, "res");
+      setInvalid(true);
+      if (e.message === "INVALID_REGISTRATION_NO_ENTERED") {
+        throw e;
+      } else {
+        throw new Error("CANNOT_REACH_REGISTRATION_NO_VALIDATION_SERVER");
+      }
+    }
+  };
 
   const useValidateno = useRequest(validateNo, {
     manual: true,
     onError: (error) => {
-      setInvalid(true)
-      props.onChange(undefined)
-      if (error.message === 'CANNOT_REACH_REGISTRATION_VALIDATION_SERVER') {
-        throw error
+      console.log("Errorr", error);
+      setInvalid(true);
+      props.onChange(undefined);
+      if (error.message === "CANNOT_REACH_REGISTRATION_NO_VALIDATION_SERVER") {
+        throw error;
       } else {
         Toast.show({
-          type: 'error',
-          position: 'bottom',
+          type: "error",
+          position: "bottom",
           props: {
-            title: translations['rNo.title'],
-            description: translations['rNo.failed']
-          }
-        })
+            title: translations["rNo.title"],
+            description: translations["rNo.failed"],
+          },
+        });
       }
     },
     onSuccess: () => {
-      setInvalid(false)
-      onChange(gstin)
+      console.log("Success444", no);
+      setInvalid(false);
+      props.onChange(no);
       Toast.show({
-        type: 'success',
-        position: 'bottom',
+        type: "success",
+        position: "bottom",
         visibilityTime: 2000,
         props: {
-          title: translations['rNo.title'],
-          description: translations['rNo.success']
-        }
-      })
-    }
-  })
+          title: translations["rNo.title"],
+          description: translations["rNo.success"],
+        },
+      });
+    },
+  });
 
   const getTickMark = () => {
+    console.log("getTickMark", props.value);
     if (!props.value) {
       if (useValidateno.loading) {
-        return (<Spinner />)
+        return <Spinner />;
       } else if (invalid) {
-        return <IconUtil.ErrorIcon />
+        return <IconUtil.ErrorIcon />;
       } else {
-        return null
+        return null;
       }
     } else {
-      return (
-        <IconUtil.CheckIcon />
-      )
+      return <IconUtil.CheckIcon />;
     }
-  }
+  };
 
   const setSelectedVehicleModel = useRequest(
     (data, dispatch) => {
@@ -152,13 +154,68 @@ const VehicleDetailsField = (props) => {
       },
     }
   );
+  // const getapi = async (no) => {
+  //   try {
+  //     console.log("registratio no", no);
+  //     const res = await axios.post(
+  //       "https://novo-adapter-uat.novopay.in/vahan-service/vahan/validate",
+  //       { rcRegnNo: no }
+  //     );
+  //     console.log("response=====>", res.data);
+  //     let response = res?.data;
+  //     if (response.status === "SUCCESS") {
+  //       console.log("inside success");
+  //       // await Promise.all([
+  //       //   dispatch.formDetails.setGstnData(response.data),
+  //       //   dispatch.formDetails.setIsGSTVerified('Yes')
+  //       // ])
+  //       // return { success: true }
+  //       Toast.show({
+  //         type: "success",
+  //         position: "bottom",
+  //         visibilityTime: 2000,
+  //         props: {
+  //           title: translations["rNo.title"],
+  //           description: translations["rNo.success"],
+  //         },
+  //       });
+  //     } else {
+  //       console.log(response.message);
+  //       Toast.show({
+  //         type: "error",
+  //         position: "bottom",
+  //         props: {
+  //           title: translations["rNo.title"],
+  //           description: translations["rNo.failed"],
+  //         },
+  //       });
+  //     }
+  //   } catch (error) {
+  //     console.log("Error", error);
+  //     console.log("inside catch", e, "res");
+  //     if (e.message === "INVALID_REGISTRATION_NO_ENTERED") {
+  //       Toast.show({
+  //         type: "error",
+  //         position: "bottom",
+  //         props: {
+  //           title: translations["rNo.title"],
+  //           description: translations["rNo.failed"],
+  //         },
+  //       });
+  //     } else {
+  //       throw new Error("CANNOT_REACH_REGISTRATION_NO_VALIDATION_SERVER");
+  //     }
+  //   }
+  // };
 
-  const onGstnChangeHandler = (no) => {
+  const onRegistrationNoChange = (no) => {
+    console.log("onRegistrationNoChange", no);
     setNo(no);
+    // getapi(input)
     if (isEmpty(no) || (no && no.length !== 10)) {
-      return
+      return;
     }
-    useValidateno.run(dispatch, no)
+    useValidateno.run(dispatch, no);
   };
   useEffect(() => {
     if (!isEmpty(selectedModel)) {
@@ -257,7 +314,7 @@ const VehicleDetailsField = (props) => {
             value={no}
             // secureTextEntry={secureEntry}
             // textContentType={textContentType}
-            onChangeText={(_, rawText) => onGstnChangeHandler(rawText)}
+            onChangeText={(_, rawText) => onRegistrationNoChange(rawText.toUpperCase())}
             // // onBlur={onBlurTextHandler}
             // onFocus={() => {
             //   onFocus(id, value)
@@ -267,45 +324,40 @@ const VehicleDetailsField = (props) => {
             // status={hasErrors && 'danger'}
             accessoryRight={() => getTickMark()}
           />
-                <View style={styles.rangeSelector}>
-
-
-          {/* <Text appearance="hint" category="label">
+          <View style={styles.rangeSelector}>
+            {/* <Text appearance="hint" category="label">
             'Valuation of the vehicle'
           </Text> */}
-          <MaskedInput
-            type="money"
-            label="Valuation of the vehicle"
-            options={{
-              precision: 0,
-              separator: ".",
-              delimiter: ",",
-              suffixUnit: "",
-            }}
-            includeRawValueInChangeText
-            // multiline={"multiline"}
-            placeholder={"Amount"}
-           
-            // editable={!disabled && !readonly}
-            keyboardType={"numeric"}
-            // value={value ? value.toString() : ""}
-            // secureTextEntry={secureEntry}
-            // textContentType={textContentType}
-            // onChangeText={(newText, rawText) =>
-            //   onChange(rawText === "" ? options.emptyValue : parseInt(rawText))
-            // }
-            // onBlur={() => {
-            //   onBlur(id, value);
-            // }}
-            // onFocus={() => {
-            //   onFocus(id, value);
-            // }}
-            selectionColor={theme.highlightColor}
-            // placeholderTextColor={theme.placeholderTextColor}
-            // status={hasErrors && "danger"}
-            // accessoryLeft={() => <Text>Rs.</Text>}
-          />
-        </View>
+            <MaskedInput
+              type="money"
+              label="Valuation of the vehicle"
+              options={{
+                precision: 0,
+                separator: ".",
+                delimiter: ",",
+                suffixUnit: "",
+              }}
+              includeRawValueInChangeText
+              // multiline={"multiline"}
+              placeholder={"Amount"}
+              // editable={!disabled && !readonly}
+              keyboardType={"numeric"}
+              value={amount}
+              // secureTextEntry={secureEntry}
+              // textContentType={textContentType}
+              onChangeText={(_,text)=>setAmount(text)}
+              // onBlur={() => {
+              //   onBlur(id, value);
+              // }}
+              // onFocus={() => {
+              //   onFocus(id, value);
+              // }}
+              selectionColor={theme.highlightColor}
+              // placeholderTextColor={theme.placeholderTextColor}
+              // status={hasErrors && "danger"}
+              // accessoryLeft={() => <Text>Rs.</Text>}
+            />
+          </View>
         </View>
       )}
     </View>
